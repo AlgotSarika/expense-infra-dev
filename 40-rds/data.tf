fe06ca7a34bb4183ca274baf8759f6eb35c4b480
Algot Sarika <sarika@joindevops.com>
2025-03-25 23:18:43 +0530

    terraform ansible

--- 40-rds/data.tf
+++ 40-rds/data.tf
@@ -1,6 +1,6 @@
 
 data "aws_ssm_parameter" "mysql_sg_id" {
-  name = "/${var.project_name}/${var.environment}/vpn_sg_id"
+  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -8,5 +8,16 @@ dnf install ansible -y
 
 #pull
 
-ansible-pull -i localhost, -U https://github.com/AlgotSarika/expense-ansible-role-tf.git main.yaml -e COMPONENT= backend -e ENVIRONMENT = $1
+ansible-pull -i localhost, -U https://github.com/AlgotSarika/expense-ansible-role-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
 
+
+
+
+# #!/bin/bash
+
+# dnf install ansible -y
+# # push
+# # ansible-playbook -i inventory mysql.yaml
+
+# #pull
+# ansible-pull  -i localhost, -U https://github.com/DAWS-82S/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
